f1a9266a0f25fbd8e9d760fe1d907bea1a1c1886
AnonyStarYM <byjichen@gmail.com>
2023-02-25 15:17:09 +0800

    002

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
-# Exercise Log
-- [001] [23.02.25] Return particular value
+# RISC-V Compiler
+This project builds a C compiler to RISC-V assembler.
+## Exercise Log
+### [23.02.25]
+- [001] Return particular value
     - Construct the RISC-V environment.
-    - Learned how to write test shell script.
-    - Learned some makefile.
+    - Learned how to write test shell script and some makefile.
+    - Learned li, ret.
+- [002] Add + -
+    - Learned strtol, addi, 
--- main.c
+++ main.c
@@ -1,28 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int Argc, char **Argv) {
-  // 判断传入程序的参数是否为2个，Argv[0]为程序名称，Argv[1]为传入的第一个参数
-  if (Argc != 2) {
-    // 异常处理，提示参数数量不对。
-    // fprintf，格式化文件输出，往文件内写入字符串
-    // stderr，异常文件（Linux一切皆文件），用于往屏幕显示异常信息
-    // %s，字符串
-    fprintf(stderr, "%s: invalid number of arguments\n", Argv[0]);
-    // 程序返回值不为0时，表示存在错误
-    return 1;
-  }
+int main(int argc, char **argv){
+    if (argc != 2) {
+      fprintf(stderr, "%s: invalid number of arguments\n", argv[0]);
+      return 1;
+    }
 
-  // 声明一个全局main段，同时也是程序入口段
-  printf("  .globl main\n");
-  // main段标签
-  printf("main:\n");
-  // li为addi别名指令，加载一个立即数到寄存器中
-  // 传入程序的参数为str类型，因为需要转换为需要int类型，
-  // atoi为“ASCII to integer”
-  printf("  li a0, %d\n", atoi(Argv[1]));
-  // ret为jalr x0, x1, 0别名指令，用于返回子程序
-  printf("  ret\n");
+    char *P = argv[1];
 
-  return 0;
+    // 声明一个全局main段，同时也是程序入口段
+    printf("  .globl main\n");
+    // main段标签
+    printf("main:\n");
+
+    // strtol为“string to long”，
+    // 参数为：被转换的str，str除去数字后的剩余部分，进制
+    // li为addi别名指令，加载一个立即数到寄存器中
+    printf("  li a0, %ld\n", strtol(P, &P, 10));
+
+    while (*P) {
+        if (*P == '+') {
+            ++P;
+            // addi rd, rs1, imm 表示 rd = rs1 + imm
+            printf("  addi a0, a0, %ld\n", strtol(P, &P, 10));
+            continue;
+        }
+        if (*P == '-') {
+            ++P;
+            // addi中imm为有符号立即数，所以减法表示为 rd = rs1 + (-imm)
+            printf("  addi a0, a0, -%ld\n", strtol(P, &P, 10));
+            continue;
+        }
+        // 如果存在未解析的字符，则报错
+        fprintf(stderr, "unexpected character: '%c'\n", *P);
+        return 1;
+    }
+
+    // ret为jalr x0, x1, 0别名指令，用于返回子程序
+    printf("  ret\n");
+
+    return 0;
 }
